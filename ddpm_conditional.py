ce9ab51d769f2c0eb049b5518c5dcfe0a4e2b8c0
Gaurav Kuppa <gkuppa@ghostrobotics.io>
2022-12-04 02:10:35 -0500

    mnist trainng

--- ddpm_conditional.py
+++ ddpm_conditional.py
@@ -117,7 +117,7 @@ def launch():
     args = parser.parse_args()
     args.run_name = "DDPM_conditional"
     args.epochs = 300
-    args.batch_size = 1
+    args.batch_size = 16
     args.image_size = 64
     args.num_classes = 10
     args.dataset_path = r"C:\Users\dome\datasets\cifar10\cifar10-64\train"
